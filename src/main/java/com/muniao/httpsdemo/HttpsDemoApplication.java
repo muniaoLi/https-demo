59a612fb18f0bb38a0f31c07f44972c2b9996de7
lixp <846358830@qq.com>
2019-08-13 13:54:26 +0800

    https demo

---
+++ src/main/java/com/muniao/httpsdemo/HttpsDemoApplication.java
@@ -0,0 +1,15 @@
+package com.muniao.httpsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(HttpsDemoApplication.class, args);
+    }
+
+}
